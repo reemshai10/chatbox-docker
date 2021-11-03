7196b266d488834378870eca413155e3005096da
reemshai <reemshai10>
2021-11-03 21:42:04 +0200

    upgrade script

--- db/db.sql
+++ db/db.sql
@@ -2,7 +2,7 @@ CREATE DATABASE mydb;
 CREATE TABLE `mydb`.`chat`(
     `room` INT,
     `user` CHAR(20) NOT NULL,
-     `msg` CHAR(20) NOT NULL,
-     `time` DATETIME,
-     PRIMARY KEY (`room`));
+    `msg` CHAR(20) NOT NULL,
+    `time` DATETIME,
+    PRIMARY KEY (`room`));
  
